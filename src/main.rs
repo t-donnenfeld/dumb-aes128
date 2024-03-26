5426a9325d6c9ee5c69302bb4f87c1dda1d2ae91
Theotime Donnenfeld <tdonnenfeld@proton.me>
2024-03-26 23:27:58 +0100

    feat: add command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "dumb-aes128")]
+#[command(version = "1.0")]
+#[command(about = "Computes aes128 with inline buffer and key", long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    input: String,
+
+    #[arg(short, long)]
+    password: String,
+
+    #[arg(short, long, default_value_t = true)]
+    encode: bool,
+}
+
 fn main() {
-    println!("Hello, AES128!");
+    let args = Cli::parse();
+    let number_of_rounds = 10;
+    let number_of_blocks = 4;
 }
